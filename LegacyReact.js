2894e3f02f741a80d1ae0668413a513a680bc75a
Bruno Henrique - Smarppy <bruno@smarppy.com>
2021-03-12 15:48:29 -0300

    Alteração do nome no app delegate.m

--- LegacyReact.js
+++ LegacyReact.js
@@ -2,17 +2,17 @@ const ReactNative = require('react-native');
 
 const { Picker } = require('@react-native-picker/picker');
 
+Object.defineProperty(ReactNative, 'Picker', {
+  value: Picker,
+  writable: false,
+});
+
 const { OS } = ReactNative.Platform;
 if (OS === 'android') {
   const {
     RNDatePickerAndroid: DatePickerAndroid, 
     RNTimePickerAndroid: TimePickerAndroid,
 } = ReactNative.NativeModules;
-
-  Object.defineProperty(ReactNative, 'AndroidDialogPicker', {
-    value: Picker,
-    writable: false,
-  });
   Object.defineProperty(ReactNative, 'DatePickerAndroid', {
       value: DatePickerAndroid,
       writable: false,
