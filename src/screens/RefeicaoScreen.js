40c09574445741a323c7c33099f5f33d8412c4f3
Daniel Kus <danielkus@hotmail.com>
2022-06-29 16:23:19 -0300

    Refeições

--- src/screens/RefeicaoScreen.js
+++ src/screens/RefeicaoScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, ScrollView, RefreshControl, Platform, Dimensions } from 'react-native';
-import { Card, Divider } from 'react-native-elements';
+import { Card, Divider, CheckBox } from 'react-native-elements';
 import { checkFormIsValid } from '../utils/Validator';
 
 import TextInput from '../components/TextInput';
@@ -22,6 +22,14 @@ export default class RefeicaoScreen extends Component {
         super(props);
 
         this.state = {
+            rhref_cod_rest: 0,
+            restaurante: [],
+            tipoRefeicao: 'CAF',
+            checkedCafe: true,
+            checkedAlmoço: false,
+            checkedJanta: false,
+            checkedMarmita: false,
+
             registro: {
                 rhref_idf: 0,
                 rhref_cod_rest: 0,
@@ -69,6 +77,11 @@ export default class RefeicaoScreen extends Component {
         this.setState({ salvado: true });
         const { registro } = this.state;
 
+        registro.rhref_cod_rest = this.state.rhref_cod_rest;
+        registro.rhref_tipo_refeicao = this.state.tipoRefeicao;
+
+        // console.log('RefeicaoScreen.onSalvar: ', registro);
+
         return axios
             .post('/refeicoes/store', registro)
             .then(response => {
@@ -78,22 +91,69 @@ export default class RefeicaoScreen extends Component {
                 const { response } = ex;
                 this.setState({ salvado: false });
 
+                // console.log('RefeicaoScreen.onSalvar.ERROR: ', ex.response);
+
                 if (ex.response) {
                     // erro no servidor
-                    Alert.showAlert('Não foi possível concluir.');
+                    Alert.showAlert('Não foi possível gravar. ' + ex.response.data);
                 } else {
                     // sem internet
-                    Alert.showAlert('Verifique sua conexão com a internet.');
+                    Alert.showAlert('Não foi possível gravar. Verifique sua conexão com a internet');
                 }
+            })
+    }
+
+
+    onEscanearPress = () => {
+        this.props.navigation.push('BarCodeScreen', {
+            onBarCodeRead: this.onBarCodeRead
+        })
+    }
+
+    onBarCodeRead = event => {
+        const { data, rawData, type } = event;
+        // console.log('RefeicaoScreen.onBarCodeRead: ', data);
+
+        this.setState({
+            rhref_cod_rest: data,
+            restaurante: [],
+        },
+            this.buscaRestaurante(data)
+        );
+    }
+
+    buscaRestaurante = (value) => {
+        this.setState({ carregando: true });
+        // console.log('RefeicaoScreen.buscaRestaurante: ', value);
+
+        axios.get('/refeicoes/buscaRestaurante/' + value)
+            .then(response => {
+                const { data } = response;
+                // console.log('RefeicaoScreen.buscaRestaurante: ', data);
+                this.setState({
+                    restaurante: data,
+                    carregando: false,
+                })
+            }).catch(error => {
+                console.warn(error);
+                console.warn(error.response);
+                this.setState({
+                    carregando: false,
+                });
             })
     }
 
 
 
 
+
+
+
     render() {
-        const { registro, salvado } = this.state;
-        const { rhref_cod_rest } = registro;
+        const { registro, restaurante, checkedCafe, checkedAlmoço, checkedJanta, checkedMarmita, salvado, carregando } = this.state;
+        const { rhref_obs } = registro;
+
+        // console.log('STATE: ', this.state)
 
         return (
             <View style={{ flex: 1, backgroundColor: Colors.background }}>
@@ -101,28 +161,153 @@ export default class RefeicaoScreen extends Component {
                     style={{ flex: 1 }}
                     keyboardShouldPersistTaps="always"
                 >
-
-                    <View
-                        style={{
-                            margin: 15,
+                    <Button
+                        title="ESCANEAR"
+                        backgroundColor={Colors.primaryLight}
+                        color={Colors.textOnPrimary}
+                        buttonStyle={{ margin: 5, marginTop: 10 }}
+                        onPress={this.onEscanearPress}
+                        icon={{
+                            name: 'qrcode',
+                            type: 'font-awesome',
+                            color: Colors.textOnPrimary
                         }}
-                    >
-                        <TextInput
-                            label="Restaurante"
-                            id="rhref_cod_rest"
-                            ref="rhref_cod_rest"
-                            value={rhref_cod_rest}
-                            maxLength={6}
-                            keyboardType="numeric"
-                            onChange={this.onInputChange}
-                        />
-                    </View>
-
-                    <ProgressDialog
-                        visible={salvado}
-                        title="SIGA PRO"
-                        message="Gravando. Aguarde..."
                     />
+
+
+
+                    <Card containerStyle={{ padding: 0, paddingVertical: 5, margin: 5, marginVertical: 7, borderRadius: 0, backgroundColor: Colors.textDisabledLight, elevation: 0, }}>
+                        <Text style={{ color: Colors.textSecondaryDark, fontSize: 25, flex: 1, fontWeight: 'bold', marginLeft: 5, color: Colors.primary }}>
+                            Restaurante
+                        </Text>
+
+                        <View style={{ paddingHorizontal: 16, paddingVertical: 8 }}>
+                            <View style={{ flexDirection: 'row', marginVertical: 2 }}>
+                                <Text style={{ color: Colors.textSecondaryDark, fontSize: 18, fontWeight: 'bold' }}>
+                                    Código: {' '}
+                                </Text>
+                                <Text style={{ color: Colors.textSecondaryDark, fontSize: 18 }} >
+                                    {restaurante.rhrest_codigo}
+                                </Text>
+                            </View>
+
+                            <View style={{ flexDirection: 'row', marginVertical: 2 }}>
+                                <Text style={{ color: Colors.textSecondaryDark, fontSize: 18, fontWeight: 'bold' }}>
+                                    Nome: {' '}
+                                </Text>
+                                <Text style={{ color: Colors.textSecondaryDark, fontSize: 18 }} >
+                                    {restaurante.adm_pes_nome}
+                                </Text>
+                            </View>
+
+                            <View style={{ flexDirection: 'row', marginVertical: 2 }}>
+                                <Text style={{ color: Colors.textSecondaryDark, fontSize: 18, fontWeight: 'bold' }}>
+                                    Cidade: {' '}
+                                </Text>
+                                <Text style={{ color: Colors.textSecondaryDark, fontSize: 18 }} >
+                                    {restaurante.ceps_loc_descricao} - {restaurante.ceps_loc_uf}
+                                </Text>
+                            </View>
+
+                        </View>
+
+                    </Card>
+
+
+                    <Card containerStyle={{ padding: 0, paddingVertical: 5, margin: 5, marginVertical: 7, borderRadius: 0, backgroundColor: Colors.textDisabledLight, elevation: 0, }}>
+                        <Text style={{ color: Colors.textSecondaryDark, fontSize: 25, flex: 1, fontWeight: 'bold', marginLeft: 5, color: Colors.primary }}>
+                            Refeição
+                        </Text>
+
+                        <View style={{ paddingHorizontal: 10, paddingVertical: 8, flexDirection: 'row' }}>
+                            <CheckBox
+                                center
+                                title='Café'
+                                checkedIcon='dot-circle-o'
+                                uncheckedIcon='circle-o'
+                                checked={checkedCafe}
+                                checkedColor={Colors.primaryLight}
+                                onPress={() => this.setState({
+                                    tipoRefeicao: 'CAF',
+                                    checkedCafe: true,
+                                    checkedAlmoço: false,
+                                    checkedJanta: false,
+                                    checkedMarmita: false,
+                                })}
+                                containerStyle={{ padding: 0, margin: 0, flex: 1, alignItems: 'flex-start', backgroundColor: 'transparent' }}
+                            />
+                            <CheckBox
+                                center
+                                title='Almoço'
+                                checkedIcon='dot-circle-o'
+                                uncheckedIcon='circle-o'
+                                checked={checkedAlmoço}
+                                checkedColor={Colors.primaryLight}
+                                onPress={() => this.setState({
+                                    tipoRefeicao: 'ALM',
+                                    checkedCafe: false,
+                                    checkedAlmoço: true,
+                                    checkedJanta: false,
+                                    checkedMarmita: false,
+                                })}
+                                containerStyle={{ padding: 0, margin: 0, flex: 1, alignItems: 'flex-start', backgroundColor: 'transparent' }}
+                            />
+                        </View>
+                        <View style={{ paddingHorizontal: 10, paddingVertical: 8, flexDirection: 'row' }}>
+                            <CheckBox
+                                center
+                                title='Jantar'
+                                checkedIcon='dot-circle-o'
+                                uncheckedIcon='circle-o'
+                                checked={checkedJanta}
+                                checkedColor={Colors.primaryLight}
+                                onPress={() => this.setState({
+                                    tipoRefeicao: 'JAN',
+                                    checkedCafe: false,
+                                    checkedAlmoço: false,
+                                    checkedJanta: true,
+                                    checkedMarmita: false,
+                                })}
+                                containerStyle={{ padding: 0, margin: 0, flex: 1, alignItems: 'flex-start', backgroundColor: 'transparent' }}
+                            />
+                            <CheckBox
+                                center
+                                title='Marmita'
+                                checkedIcon='dot-circle-o'
+                                uncheckedIcon='circle-o'
+                                checked={checkedMarmita}
+                                checkedColor={Colors.primaryLight}
+                                onPress={() => this.setState({
+                                    tipoRefeicao: 'MAR',
+                                    checkedCafe: false,
+                                    checkedAlmoço: false,
+                                    checkedJanta: false,
+                                    checkedMarmita: true,
+                                })}
+                                containerStyle={{ padding: 0, margin: 0, flex: 1, alignItems: 'flex-start', backgroundColor: 'transparent' }}
+                            />
+                        </View>
+
+                    </Card>
+                    <Card containerStyle={{ padding: 0, paddingVertical: 5, margin: 5, marginVertical: 7, borderRadius: 0, backgroundColor: Colors.textDisabledLight, elevation: 0, }}>
+                        <Text style={{ color: Colors.textSecondaryDark, fontSize: 25, flex: 1, fontWeight: 'bold', marginLeft: 5, color: Colors.primary }}>
+                            Observação
+                        </Text>
+                        <View style={{ paddingHorizontal: 10, marginTop: 10 }}>
+                            <TextInput
+                                label=""
+                                id="rhref_obs"
+                                ref="rhref_obs"
+                                value={rhref_obs}
+                                onChange={this.onInputChange}
+                                multiline={true}
+                                style={{ height: 70, }}
+                            />
+                        </View>
+                    </Card>
+
+
+
 
                 </ScrollView >
 
@@ -137,6 +322,18 @@ export default class RefeicaoScreen extends Component {
                         type: 'font-awesome',
                         color: Colors.textOnPrimary
                     }}
+                />
+
+                <ProgressDialog
+                    visible={salvado}
+                    title="SIGA PRO"
+                    message="Gravando. Aguarde..."
+                />
+
+                <ProgressDialog
+                    visible={carregando}
+                    title="SIGA PRO"
+                    message="Carregando..."
                 />
 
             </View>
