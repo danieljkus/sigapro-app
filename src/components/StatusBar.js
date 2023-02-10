36f29146c8b3353e15462db7b7c1b8a2ba5bd436
mmayk.felix@gmail.com <mmayk.felix@gmail.com>
2023-02-10 20:11:33 -0300

    Fixes :  Statusbar android / ios

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import { Platform, StatusBar as SB } from 'react-native';
-const { OS } = Platform;
+import {Platform, StatusBar as SB} from 'react-native';
+
+const {OS} = Platform;
 
 import Colors from '../values/Colors';
 
 const StatusBar = (props) => {
     return (
-        <SB {...props} />
+        <SB
+            animated={props?.animated}
+            backgroundColor={props?.backgroundColor}
+            showHideTransition={props?.showHideTransition}
+            barStyle={props?.barStyle}
+            translucent={props?.translucent}
+            {...props}
+        />
     )
-}
+};
 
 StatusBar.propTypes = {
     ...SB.propTypes
 };
 
 StatusBar.defaultProps = {
-    backgroundColor: Colors.primaryDark,
+    animated: true,
+    backgroundColor: '#1F829C',
+    showHideTransition: 'fade',
     barStyle: OS === 'ios' ? 'dark-content' : 'light-content',
     translucent: false,
-}
+};
 
 export default StatusBar;
