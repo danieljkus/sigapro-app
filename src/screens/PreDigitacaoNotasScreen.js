605a54b40a3cb1c12ed98d5df3512b75c6dbaee6
daniel <danielkus@hotmail.com>
2023-08-10 17:01:26 -0300

    Notas Fiscais

--- src/screens/PreDigitacaoNotasScreen.js
+++ src/screens/PreDigitacaoNotasScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Alert, FlatList, Platform, TouchableOpacity, ActivityIndicator, SafeAreaView } from 'react-native';
+import { View, Text, Alert, FlatList, Platform, TouchableOpacity, ActivityIndicator, SafeAreaView, Modal, ScrollView } from 'react-native';
 const { OS } = Platform;
 
 import moment from 'moment';
@@ -9,16 +9,20 @@ import FloatActionButton from '../components/FloatActionButton';
 import Colors from '../values/Colors';
 import { ProgressDialog } from 'react-native-simple-dialogs';
 import HeaderComponent from "../components/HeaderComponent";
+import TextInput from '../components/TextInput';
+import { maskDate, maskValorMoeda } from '../utils/Maskers';
+import Button from '../components/Button';
+import { getFilial } from '../utils/LoginManager';
 
 const SwitchStyle = OS === 'ios' ? { transform: [{ scaleX: .7 }, { scaleY: .7 }] } : undefined;
-
+const DATE_FORMAT = 'DD/MM/YYYY';
 
 const CardViewItem = ({ registro, onRegistroLongPress }) => {
     return (
         <Card containerStyle={{ padding: 0, margin: 0, marginVertical: 7, borderRadius: 0, backgroundColor: Colors.textDisabledLight, elevation: 0, }}>
             <View style={{ borderLeftWidth: 5, borderLeftColor: registro.estoq_nfpd_sit_nfe === 'PEN' ? "#d32f2f" : registro.estoq_nfpd_sit_nfe === 'BAI' ? "#DAA520" : Colors.primary }}>
                 <TouchableOpacity
-                    onLongPress={() => onRegistroLongPress(registro.estoq_nfpd_chave)}
+                // onLongPress={() => onRegistroLongPress(registro.estoq_nfpd_chave)}
                 >
                     <View style={{ paddingLeft: 10, marginTop: 5, fontSize: 13, flexDirection: 'row' }}>
                         <View style={{ flex: 2, flexDirection: 'row' }}>
@@ -61,7 +65,7 @@ const CardViewItem = ({ registro, onRegistroLongPress }) => {
                                 Valor {': '}
                             </Text>
                             <Text>
-                                {registro.estoq_nfpd_valor_nfe}
+                                {maskValorMoeda(parseFloat(registro.estoq_nfpd_valor_nfe))}
                             </Text>
                         </View>
                     </View>
@@ -141,12 +145,27 @@ export default class PreDigitacaoNotasScreen extends Component {
         refreshing: false,
         carregando: false,
         carregarMais: false,
+        buscandoNotas: false,
         pagina: 1,
+
+        modalFiltrosVisible: false,
+        // dataIni: moment(moment().subtract(2, 'days')).format(DATE_FORMAT),
+        dataIni: moment(new Date()).format(DATE_FORMAT),
+        dataFim: moment(new Date()).format(DATE_FORMAT),
+        filial: 0,
+        nfe: '',
+        cnpj: '',
     };
 
     componentDidMount() {
-        this.setState({ refreshing: false });
-        this.getListaRegistros();
+        getFilial().then(filial => {
+            this.setState({
+                filial,
+                refreshing: false,
+            }, this.getListaRegistros());
+        })
+        // this.setState({ refreshing: false });
+        // this.getListaRegistros();
     }
 
     onRefresh = () => {
@@ -157,13 +176,19 @@ export default class PreDigitacaoNotasScreen extends Component {
     }
 
     getListaRegistros = () => {
-        const { pagina, listaRegistros } = this.state;
+        const { dataIni, dataFim, filial, nfe, cnpj, pagina, listaRegistros } = this.state;
         this.setState({ refreshing: true, });
 
         axios.get('/preDigitacaoNotas', {
             params: {
                 page: pagina,
                 limite: 10,
+
+                dtIni: moment(dataIni, DATE_FORMAT).format("YYYY-MM-DD"),
+                dtFim: moment(dataFim, DATE_FORMAT).format("YYYY-MM-DD"),
+                filial,
+                nfe,
+                cnpj,
             }
         }).then(response => {
             const novosRegistros = pagina === 1
@@ -185,11 +210,46 @@ export default class PreDigitacaoNotasScreen extends Component {
         })
     }
 
+    onInputChange = (id, value) => {
+        const state = {};
+        state[id] = value;
+        this.setState(state);
+    }
+
+
     onAddPress = () => {
         this.props.navigation.navigate('PreDigitacaoNotaScreen', {
             estoq_nfpd_chave: '',
             onRefresh: this.onRefresh
         });
+    }
+
+
+
+    onBuscarNotasPress = () => {
+        const { dataIni, dataFim } = this.state;
+        this.setState({ buscandoNotas: true, refreshing: true, });
+
+        axios.get('/notasFiscais/buscaNotas', {
+            params: {
+                date_ini: moment(dataIni, DATE_FORMAT).format("YYYY-MM-DD"),
+                date_end: moment(dataFim, DATE_FORMAT).format("YYYY-MM-DD"),
+            }
+        }).then(response => {
+
+            // console.log('onBuscarNotasPress: ', response)
+
+            this.setState({ buscandoNotas: false, refreshing: true, });
+            this.getListaRegistros();
+
+        }).catch(ex => {
+            console.warn('Erro Busca Notas:', ex);
+            this.setState({
+                buscandoNotas: false,
+                refreshing: false,
+                carregando: false,
+            });
+        })
     }
 
 
@@ -229,7 +289,6 @@ export default class PreDigitacaoNotasScreen extends Component {
 
     carregarMaisRegistros = () => {
         const { carregarMais, refreshing, carregando, pagina } = this.state;
-
         if (carregarMais && !refreshing && !carregando) {
             this.setState({
                 carregando: true,
@@ -261,8 +320,31 @@ export default class PreDigitacaoNotasScreen extends Component {
         )
     }
 
+
+    onSearchPress = (visible) => {
+        this.setState({ modalFiltrosVisible: visible });
+        this.setState({
+            pagina: 1,
+            refreshing: true,
+        }, this.getListaRegistros);
+    }
+
+    onClosePress = (visible) => {
+        this.setState({ modalFiltrosVisible: visible });
+    }
+
+
+
+
+
+
+
+
+
     render() {
-        const { listaRegistros, refreshing, carregando } = this.state;
+        const { listaRegistros, dataIni, dataFim, filial, nfe, cnpj,
+            refreshing, carregando, buscandoNotas } = this.state;
+
         return (
             <SafeAreaView style={{ backgroundColor: Colors.background, flex: 1 }}>
                 <HeaderComponent
@@ -282,12 +364,174 @@ export default class PreDigitacaoNotasScreen extends Component {
                     ListFooterComponent={this.renderListFooter}
                 />
 
+
+                <FloatActionButton
+                    iconFamily="MaterialIcons"
+                    iconName="search"
+                    iconColor={Colors.textOnPrimary}
+                    onPress={() => { this.onSearchPress(true) }}
+                    backgroundColor={Colors.primary}
+                    marginBottom={158}
+                    marginRight={10}
+                />
+
                 <FloatActionButton
                     iconFamily="MaterialIcons"
                     iconName="add"
                     iconColor={Colors.textOnAccent}
                     onPress={this.onAddPress}
                     backgroundColor={Colors.primary}
+                    marginBottom={90}
+                    marginRight={10}
+                />
+
+                <FloatActionButton
+                    iconFamily="MaterialIcons"
+                    iconName="cloud-download"
+                    iconColor={Colors.textOnAccent}
+                    onPress={this.onBuscarNotasPress}
+                    backgroundColor={Colors.primary}
+                    marginRight={10}
+                />
+
+
+
+                {/* ----------------------------- */}
+                {/* MODAL PARA FILTROS            */}
+                {/* ----------------------------- */}
+                <Modal
+                    visible={this.state.modalFiltrosVisible}
+                    animationType={"slide"}
+                    transparent={true}
+                >
+                    <View style={{
+                        backgroundColor: 'rgba(0,0,0,0.5)',
+                        flex: 1,
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}>
+                        <View style={{
+                            width: "90%",
+                        }}>
+                            <View style={{
+                                paddingVertical: 15,
+                                paddingHorizontal: 15,
+                                backgroundColor: Colors.background,
+                                borderRadius: 5,
+                            }}>
+
+                                <View style={{ backgroundColor: Colors.primary, flexDirection: 'row' }}>
+                                    <Text style={{
+                                        color: Colors.textOnPrimary,
+                                        marginTop: 15,
+                                        marginBottom: 15,
+                                        marginLeft: 16,
+                                        textAlign: 'center',
+                                        fontSize: 20,
+                                        fontWeight: 'bold',
+                                    }}>Filtrar</Text>
+                                </View>
+
+                                <View style={{ marginTop: 4, paddingVertical: 10 }}>
+
+                                    <ScrollView style={{ height: 50, width: "100%", marginBottom: 10 }}>
+                                        <View style={{ flexDirection: 'row' }}>
+                                            <View style={{ width: "47%", marginRight: 20 }}>
+                                                <TextInput
+                                                    type="date"
+                                                    label="Data Início"
+                                                    id="dataIni"
+                                                    ref="dataIni"
+                                                    value={dataIni}
+                                                    masker={maskDate}
+                                                    dateFormat={DATE_FORMAT}
+                                                    onChange={this.onInputChange}
+                                                    validator={data => moment(data, "DD/MM/YYYY", true).isValid()}
+                                                    fontSize={12}
+                                                />
+                                            </View>
+                                            <View style={{ width: "47%" }}>
+                                                <TextInput
+                                                    type="date"
+                                                    label="Data Fim"
+                                                    id="dataFim"
+                                                    ref="dataFim"
+                                                    value={dataFim}
+                                                    masker={maskDate}
+                                                    dateFormat={DATE_FORMAT}
+                                                    onChange={this.onInputChange}
+                                                    validator={data => moment(data, "DD/MM/YYYY", true).isValid()}
+                                                    fontSize={12}
+                                                />
+                                            </View>
+                                        </View>
+                                    </ScrollView>
+
+
+                                    <TextInput
+                                        label="Filial"
+                                        id="filial"
+                                        ref="filial"
+                                        value={filial}
+                                        maxLength={10}
+                                        onChange={this.onInputChange}
+                                        keyboardType="numeric"
+                                    />
+
+                                    <TextInput
+                                        label="Numero NFe"
+                                        id="nfe"
+                                        ref="nfe"
+                                        value={nfe}
+                                        maxLength={10}
+                                        onChange={this.onInputChange}
+                                        keyboardType="numeric"
+                                    />
+
+                                    <TextInput
+                                        label="CNPJ"
+                                        id="cnpj"
+                                        ref="cnpj"
+                                        value={cnpj}
+                                        maxLength={10}
+                                        onChange={this.onInputChange}
+                                        keyboardType="numeric"
+                                    />
+
+
+
+                                    <Button
+                                        title="FILTRAR"
+                                        onPress={() => { this.onSearchPress(!this.state.modalFiltrosVisible) }}
+                                        buttonStyle={{ marginTop: 10 }}
+                                        backgroundColor={Colors.buttonPrimary}
+                                        icon={{
+                                            name: 'filter',
+                                            type: 'font-awesome',
+                                            color: Colors.textOnPrimary
+                                        }}
+                                    />
+                                    <Button
+                                        title="FECHAR"
+                                        onPress={() => { this.onClosePress(!this.state.modalFiltrosVisible) }}
+                                        buttonStyle={{ marginTop: 10 }}
+                                        backgroundColor={Colors.buttonPrimary}
+                                        icon={{
+                                            name: 'close',
+                                            type: 'font-awesome',
+                                            color: Colors.textOnPrimary
+                                        }}
+                                    />
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
+
+                <ProgressDialog
+                    visible={buscandoNotas}
+                    title="SIGA PRO"
+                    message="Buscando Notas Fiscais. Isso pode levar alguns minutos. Aguarde..."
                 />
             </SafeAreaView>
         )
